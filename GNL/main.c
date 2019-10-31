12e3b812cbecde7bf46ae9f056999b553f6bcad8
T-PRAT <Titouan.p@hotmail.fr>
2019-10-31 15:23:08 +0100

    debut GNL

---
+++ GNL/main.c
@@ -0,0 +1,16 @@
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main()
+{
+	int fd;
+	char **line;
+
+	if (!(fd = open("file", O_RDONLY)))
+		printf("cant open file");
+	if (get_next_line(fd, line) == -1)
+		printf("error");
+	printf("%s\n", *line);
+}
