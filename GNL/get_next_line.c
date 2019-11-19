7154e15b0d40c9fcb094896e275fe6c9921445e0
Titouan Prat <tprat@z1r1p4.le-101.fr>
2019-11-19 20:32:23 +0100

    1

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tprat <marvin@le-101.fr>                   +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/04 19:16:32 by tprat        #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/19 19:21:45 by tprat       ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/19 20:26:58 by tprat       ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,7 +18,6 @@ int		get_next_line(int fd, char **line)
 	int			rsize;
 	char		buf[BUFFER_SIZE];
 	static char		rest[BUFFER_SIZE];
-
 
 	if ((rsize = read(fd, buf, BUFFER_SIZE)) == -1)
 		return (-1);
