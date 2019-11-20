35dd991b60690057cacec494cb8acabfa4dc352b
Titouan Prat <tprat@z1r1p4.le-101.fr>
2019-11-20 21:01:58 +0100

    structure faite

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,20 +6,39 @@
 /*   By: tprat <marvin@le-101.fr>                   +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/04 19:16:32 by tprat        #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/19 20:26:58 by tprat       ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/20 21:01:37 by tprat       ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+
+
 int		get_next_line(int fd, char **line)
 {
 	int			rsize;
-	char		buf[BUFFER_SIZE];
-	static char		rest[BUFFER_SIZE];
+	int			v;
+	static char	*buf;
+	char		*tmp;
 
-	if ((rsize = read(fd, buf, BUFFER_SIZE)) == -1)
+	if (fd < 0)
 		return (-1);
+	if (!(buf))
+	{
+		if (!(buf = calloc(BUFFER_SIZE, sizeof(char))))
+			return (-1);
+	}
+	while (rsize)
+	{
+		if (!(buf[0]))
+		{
+			line = ft_strdup(buf);
+			tmp = buf;
+			buf = ft_strdup(ft_strchr(buf, '\n', 
+		}
+		if ((rsize = read(fd, buf, BUFFER_SIZE)) == -1)
+			return (-1);
+	}
 	return (1);
 }
--- GNL/get_next_line_utils.c
+++ GNL/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: tprat <marvin@le-101.fr>                   +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/06 16:11:12 by tprat        #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/19 19:21:19 by tprat       ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/20 21:01:39 by tprat       ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -50,4 +50,60 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	}
 	str[j] = 0;
 	return (str);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char	*str;
+	size_t	len;
+
+	len = size * count;
+	if (!(str = malloc(len)))
+		return (0);
+	while (len > 0)
+	{
+		len--;
+		str[len] = 0;
+	}
+	return ((void *)str);
+}
+
+char	*ft_strdup(const char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	if (!(dest = malloc((i + 1) * sizeof(char))))
+		return (0);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	int		i;
+	char	*str2;
+
+	str2 = (char *)str;
+	i = 0;
+	while (str[i] != c && str[i])
+		i++;
+	if (str[i] == 0)
+	{
+		if (c == 0)
+			return (str2 + i);
+		else
+			return (0);
+	}
+	else
+		return (str2 + i);
 }
