ee3a593664b25c1a17c8d2ec6ac6cdd79312dfd1
Titouan Prat <tprat@z1r9p4.le-101.fr>
2019-11-21 19:40:35 +0100

    oui

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,20 +6,18 @@
 /*   By: tprat <marvin@le-101.fr>                   +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/04 19:16:32 by tprat        #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/20 21:01:37 by tprat       ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/21 19:39:32 by tprat       ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
-
 int		get_next_line(int fd, char **line)
 {
 	int			rsize;
 	int			v;
-	static char	*buf;
+	static char	buf[BUFFER_SIZE];
 	char		*tmp;
 
 	if (fd < 0)
@@ -31,11 +29,12 @@ int		get_next_line(int fd, char **line)
 	}
 	while (rsize)
 	{
-		if (!(buf[0]))
+		if (buf[0])
 		{
-			line = ft_strdup(buf);
+			*line = ft_strdup(buf);
 			tmp = buf;
-			buf = ft_strdup(ft_strchr(buf, '\n', 
+			buf = ft_strdup(ft_strchr(buf, '\n'));
+			free(tmp);
 		}
 		if ((rsize = read(fd, buf, BUFFER_SIZE)) == -1)
 			return (-1);
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,5 +6,9 @@
 #include <unistd.h>
 
 int		get_next_line(int fd, char **line);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *src);
+char	*ft_strchr(const char *str, int c);
 
 #endif
